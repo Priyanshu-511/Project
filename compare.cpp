b963219432cef8b37b396b54151de29b764aed7f
Priyanshu <161984213+Priyanshu-511@users.noreply.github.com>
2026-04-30 10:17:40 +0530

    Update compare.cpp with new content

---
+++ compare.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<sstream>
+using namespace std;
+
+int main(){
+    string str1,str2;
+    ifstream out1,out2;
+    out1.open("output.txt");
+    // FIXED: Use getline properly instead of eof() - eof() is unreliable
+    while(getline(out1,str1)){
+        // Read last line
+    }
+    out1.close();
+    
+out2.open("run.txt");
+    // FIXED: Use getline properly instead of eof() - eof() is unreliable
+    while(getline(out2,str2)){
+        // Read last line
+    }
+    out2.close();
+
+    // FIXED: Removed unused stringstream objects s1 and s2
+    
+    if(str1==str2){
+        cout<<"Test-case passed:\n";
+    }
+    else{
+        cout<<"Test-case failed:\n";
+    }
+}
